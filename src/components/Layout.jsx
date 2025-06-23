cfa2f30cbd9c9053c22546df351771210343d38c
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-06-23 06:25:56 +0300

    create profile  dashboard

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from './Footer';
 import Navbar from './Navbar';
-import { About, Pricing, Profile, Contact } from '@/pages';
-import TemplateGrid from '@/pages/TemplateGrid';
-import { Home} from '../pages';
-import FAQs from '@/pages/Faqs';
 
 
 export default function Layout() {
