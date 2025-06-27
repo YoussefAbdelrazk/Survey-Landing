b3f61496b08e0b9b3d8394bb057eb7242a6d63a7
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-06-27 15:44:23 +0300

    hide footer in sign-in/sign-up pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import { Footer } from './Footer';
-import Navbar from './Navbar';
-
+import { Outlet, useLocation } from "react-router-dom";
+import { Footer } from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout() {
+  const location = useLocation();
+  const hideFooterRoutes = ["/sign-in", "/sign-up"];
+
+  const shouldHideFooter = hideFooterRoutes.includes(location.pathname);
+
   return (
     <div>
       <Navbar />
-      <br/>
-      <Outlet/>
-      <br/>
-      <hr/>
-      <Footer />
+      <br />
+      <Outlet />
+      <br />
+      <hr />
+      {/* Conditionally render the Footer */}
+      {!shouldHideFooter && <Footer />}
     </div>
   );
 }
